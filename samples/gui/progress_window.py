ed0e9f7d23c0f6f5712f5c35356711296efa3cb0
Klaassen, Sebastian <klaassens@ornl.gov>
2018-07-31 09:17:15 -0400

    Added sample

---
+++ samples/gui/progress_window.py
@@ -0,0 +1,60 @@
+import os.path
+import sys
+sys.path.append(os.path.realpath('.'))
+from asyncframes import run, Frame, sleep
+from gui import WFrame, Button, ProgressBar, Layout
+
+@Frame
+async def main():
+	# Start an arbitrary process in the background
+	p = process()
+	# Wait until the process has started reporting progress
+	await sleep() #await (lambda: hasattr(p, 'progress')) #TODO
+
+	# Show a wframe to monitor progress and wait until it finishes
+	await dialog(p)
+
+	# If the process hasn't finished already, destroy it manually
+	# Note: This function isn't necessary here, since any frame is automatically removed when it goes out of scope.
+	p.remove()
+
+@Frame
+async def process(self):
+	"""
+	This frame simulates a time consuming process (i.e. a file copy operation).
+	It reports information about its own progress through the self.progress variable (range: 0 to 100).
+	The frame exits once the process is completed.
+	This process simply advances its progress by 1% every 100ms (total runtime: 100 * 100ms = 10s)
+	"""
+	self.progress = 0 # Progress in %
+	while self.progress < 100:
+		self.progress += 1
+		await sleep(0.1)
+
+@Frame
+async def monitor_progress(p, pb):
+	"""
+	This frame synchronizes progress with the value of a progress bar.
+	It exits once progress reaches 100%.
+	"""
+	while p.progress < 100:
+		pb.value = p.progress
+		await sleep() # Make this a non-blocking loop
+	pb.value = 100
+
+@WFrame(size=(400, 50), layout=Layout.hbox)
+async def dialog(p):
+	"""
+	This frame shows a progress dialog with a progress bar and a cancel button.
+	It exits once the monitored process finishes or the cancel button is pressed.
+	"""
+	# Create dialog controls
+	pg_progress = ProgressBar()
+	cmd_cancel = Button("Cancel")
+
+	# Wait until either monitor_progress finishes or cmd_cancel is clicked
+	await monitor_progress(p, pg_progress) #await (monitor_progress(p, pg_progress) | cmd_cancel.click) #TODO
+
+	# Close dialog
+
+run(main)
