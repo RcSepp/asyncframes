35d85830ffe080c28f67dc20273d0cc343443ff9
Klaassen, Sebastian <klaassens@ornl.gov>
2018-09-04 16:20:54 -0400

    Added version and long_description

--- setup.py
+++ setup.py
@@ -2,13 +2,22 @@
 # Copyright (c) Sebastian Klaassen. All Rights Reserved.
 # Distributed under the MIT License. See LICENSE file for more info.
 
-from setuptools import setup
+import os.path
+import setuptools
+import asyncframes
 
-setup(
+def read(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
+        return file.read()
+
+ld = read('README.rst') + '\n\n' + read('CHANGES.rst')
+
+setuptools.setup(
     name='asyncframes',
     packages=['asyncframes'],
-    version='0.1',
+    version=asyncframes.__version__,
     description='Object oriented coroutines for everyone',
+    long_description=read('README.rst') + '\n\n' + read('CHANGES.rst'),
     author='RcSepp',
     author_email='rcsepp@hotmail.com',
     license='MIT'
